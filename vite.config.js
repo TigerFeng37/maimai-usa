512fb7adb7722b41d7af52d86f90ee64662293df
FENGUY <me@feng.works>
2025-09-25 01:36:35 -0400

    CF deployment test 3

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/',  // Set base path for GitHub Pages
+  base: './',  // Set base path for Cloudflare Pages (relative paths)
 })
