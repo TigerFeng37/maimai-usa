24a6c2b7d3f111072541503db62a0fb53c2af62b
FENGUY <me@feng.works>
2025-10-22 18:13:17 -0400

    fix button

--- src/components/RecentsBanner.jsx
+++ src/components/RecentsBanner.jsx
@@ -3,13 +3,14 @@ import Locations from '../recent-locations.json';
 function RecentsBanner() {
     return (
         <>
-            <div className="flex flex-row items-center gap-2 p-4">
+            <div className="flex flex-col md:flex-row items-start gap-2 py-2 px-4 w-full">
                 <span className="text-sm text-gray-500">Recently Activated</span>
-                <div className="flex flex-row items-center gap-2">
+                <div className="w-full md:w-fit flex flex-col md:flex-row items-start gap-1 md:gap-2 justify-start">
                     {Locations.map((location) => (
-                        <button key={location.code} className="text-sm font-medium text-black dark:text-white" onClick={() => navigate(`/location/${location.code}`)}>
+                        <a key={location.code} className="w-full md:w-fit text-sm font-medium text-black dark:text-white flex flex-row items-center gap-1 justify-between md:justify-start" onClick={() => navigate(`/location/${location.code}`)} href={`/location/${location.code}`}>
                             {location.name}
-                        </button>
+                            <span className="text-sm text-gray-500 md:hidden">→</span>
+                        </a>
                     ))}
                 </div>
             </div>
