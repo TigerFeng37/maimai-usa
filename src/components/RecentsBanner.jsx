c15f21e47b1e695d4b1846e3dc9551ba7188746f
FENGUY <me@feng.works>
2025-10-22 18:07:34 -0400

    recent locations banner

--- src/components/RecentsBanner.jsx
+++ src/components/RecentsBanner.jsx
@@ -7,7 +7,7 @@ function RecentsBanner() {
                 <span className="text-sm text-gray-500">Recently Activated</span>
                 <div className="flex flex-row items-center gap-2">
                     {Locations.map((location) => (
-                        <button key={location.code} className="text-sm text-white">
+                        <button key={location.code} className="text-sm font-medium text-black dark:text-white">
                             {location.name}
                         </button>
                     ))}
